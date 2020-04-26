61e1fd45ddcb5b42a58ed402ff7dc1612a3f0dbd
dotgital <support@dotgital.com>
2020-04-25 20:03:50 -0500

    v0.13

--- api/clients/controllers/clients.js
+++ api/clients/controllers/clients.js
@@ -57,28 +57,39 @@ module.exports = {
   },
 
 
-    async create(ctx) {
-        let entity;
-        const currentUser = ctx.state.user;
-        if (ctx.is('multipart')) {
-          const { data, files } = parseMultipartData(ctx);
-          ctx.data.createdBy = currentUser.id;
-          entity = await strapi.services.clients.create(data, { files });
-        } else {
-          ctx.request.body.createdBy = currentUser.id;
-          entity = await strapi.services.clients.create(ctx.request.body);
-        }
-        return sanitizeEntity(entity, { model: strapi.models.clients });
-    },
+  async create(ctx) {
+    let entity;
+    console.log(typeof ctx.request.body.assignedTo);
+    if (typeof ctx.request.body.assignedTo === 'string'){
+      ctx.request.body.assignedTo = [];
+    }
+    ctx.request.body.recordName = `${ctx.request.body.firstName} ${ctx.request.body.lastName}`
 
-    // async update(ctx){
-    //   let entities;
-    //   if (ctx.query._q) {
-    //     entities = await strapi.services.users.search(ctx.query);
-    //   } else {
-    //     entities = await strapi.services.users.find(ctx.query);
-    //   }
+    const currentUser = ctx.state.user;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      ctx.data.createdBy = currentUser.id;
+      entity = await strapi.services.clients.create(data, { files });
+    } else {
+      ctx.request.body.createdBy = currentUser.id;
+      entity = await strapi.services.clients.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.clients });
+  },
 
-    //   return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.restaurant }));
-    // },
+  async update(ctx){
+    const { id } = ctx.params;
+    ctx.request.body.recordName = `${ctx.request.body.firstName} ${ctx.request.body.lastName}`
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.clients.update({ id }, data, {
+        files,
+      });
+    } else {
+      entity = await strapi.services.clients.update({ id }, ctx.request.body);
+    }
+
+    return sanitizeEntity(entity, { model: strapi.models.clients });
+  },
 };
--- api/fields-settings/models/fields-settings.js
+++ api/fields-settings/models/fields-settings.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /**
- * Lifecycle callbacks for the `settings` model.
+ * Lifecycle callbacks for the `fields-settings` model.
  */
 
 module.exports = {
