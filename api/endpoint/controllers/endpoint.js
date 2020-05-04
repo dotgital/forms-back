e756a37e617b1507653845d4378f74fc8983d962
dotgital <support@dotgital.com>
2020-05-03 22:10:56 -0500

    v0.67

--- api/endpoint/controllers/endpoint.js
+++ api/endpoint/controllers/endpoint.js
@@ -25,5 +25,46 @@ module.exports = {
     }
 
     return entity;
-  }
+  },
+
+  async list(ctx) {
+    let entities;
+    let count;
+    const module = ctx.params.module;
+    const columns = ctx.request.body.columns
+    const permissionId = ctx.state.user.custom_permission
+    const permissions = await strapi.services['custom-permissions'].findOne({ id: permissionId });
+
+    if (permissions.clients_view === 'onlyAssigned'){
+      ctx.query = {
+        ...ctx.query,
+        assignedTo_in: [ctx.state.user.id]
+      };
+    } else if (permissions.clients_view === 'None') {
+      ctx.response.forbidden({message: `You don't have access to this records`});
+      return ctx.response;
+    }
+
+    if(module === 'users'){
+      delete ctx.query.assignedTo_in;
+      count = await strapi.query('user', 'users-permissions').count()
+      entities = await strapi.plugins['users-permissions'].services.user.fetchAll(ctx.query);
+    } else {
+      count = await strapi.services[module].count(ctx.query);
+      entities = await strapi.services[module].find(ctx.query)
+    }
+
+    entities = entities.map(entity => {
+      Object.keys(entity).forEach(key => {
+        if(!columns.includes(key)){
+          delete entity[key]
+        }
+      })
+      return entity;
+    });
+
+    entities = await Promise.all(entities);
+
+    return {count, entities};
+  },
 };
