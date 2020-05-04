93c88a0f758bb7b58cd46ac7f3661c9bbc0c7d99
dotgital <support@dotgital.com>
2020-05-04 17:54:48 -0500

    v0.67

--- api/endpoint/controllers/endpoint.js
+++ api/endpoint/controllers/endpoint.js
@@ -63,6 +63,7 @@ module.exports = {
           if(Array.isArray(entity[key])){
             entity[key] = entity[key].map(ent => {
               const {id, recordName} = ent
+              // return ` ${recordName}`
               return {id, recordName}
             });
           }
